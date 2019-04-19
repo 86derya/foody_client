62d714f8628d74286838cebf8577bd2cee5ae3dc
Andrii Dederia <derya_86@ukr.net>
2019-04-19 13:54:09 +0300

    build created

---
+++ build/static/js/Home-page.d7f7891b.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{141:function(n,e,t){"use strict";t.r(e);var a=t(0),o=t.n(a);e.default=function(){return o.a.createElement("h1",null," Home page")}}}]);
+//# sourceMappingURL=Home-page.d7f7891b.chunk.js.map
